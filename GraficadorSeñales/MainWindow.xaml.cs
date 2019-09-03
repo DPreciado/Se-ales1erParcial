d0e4bd12e462e8f8895ad455e5d4a1241982922d
david arturo preciado osuna <arturo_preciado@live.com.mx>
2019-09-03 10:36:41 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraficadorSeñales
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void BtnGraficar_Click(object sender, RoutedEventArgs e)
+        {
+            double amplitud = double.Parse(txtbox_amplitud.Text);
+            double fase = double.Parse(txtbox_fase.Text);
+            double frecuencia = double.Parse(txtbox_frecuencia.Text);
+            double tiempoInicial = double.Parse(txtbox_tiempoInicial.Text);
+            double tiempoFinal = double.Parse(txtbox_tiempoFinal.Text);
+            double muestro = double.Parse(txtbox_muestreo.Text);
+
+            // SeñalSenoidal señal = new SeñalSenoidal(amplitud, fase, frecuencia);
+           // SeñalParabolica señal = new SeñalParabolica();
+            SeñalIdentidad señal = new SeñalIdentidad();
+
+            double periodoMuestreo = 1.0 / muestro;
+
+            double amplitudMaxima = 0.0;
+
+            plnGrafica.Points.Clear();
+            for (double i = tiempoInicial; i <= tiempoFinal; i += periodoMuestreo)
+            {
+                double valorMuestra = señal.evaluar(i);
+                if(Math.Abs(valorMuestra) > amplitudMaxima)
+                {
+                    amplitudMaxima = Math.Abs(valorMuestra);
+                }
+                Muestra muestra = new Muestra(i, señal.evaluar(i));
+                señal.Muestras.Add(muestra);
+            }
+
+            foreach(Muestra muestra in señal.Muestras)
+            {
+                plnGrafica.Points.Add(adaptarCoordenadas(muestra.X, muestra.Y, tiempoInicial, amplitudMaxima));
+            }
+
+            lblLimiteSuperior.Text = amplitudMaxima.ToString();
+            lblLimiteInferior.Text = "-" + amplitudMaxima.ToString();
+
+            plnEjeX.Points.Clear();
+            plnEjeX.Points.Add(adaptarCoordenadas(tiempoInicial,0.0,tiempoInicial, amplitudMaxima));
+            plnEjeX.Points.Add(adaptarCoordenadas(tiempoFinal, 0.0,tiempoInicial, amplitudMaxima));
+
+            plnEjeY.Points.Clear();
+            plnEjeY.Points.Add(adaptarCoordenadas(0.0, amplitudMaxima, tiempoInicial, amplitudMaxima));
+            plnEjeY.Points.Add(adaptarCoordenadas(0.0, -amplitudMaxima, tiempoInicial, amplitudMaxima));
+        }
+
+        public Point adaptarCoordenadas(double x, double y, double tiempoInicial, double amplitudMaxima)
+        {
+            return new Point((x - tiempoInicial) * scrGrafica.Width,
+                (-1 * (
+                y * (((scrGrafica.Height / 2.0) -25) / amplitudMaxima)) +
+                (scrGrafica.Height / 2.0) ));
+        }
+    }
+}
